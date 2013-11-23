28e66be41b8bf10ceb55c06edff0d2a5e960c0b7
George Butiri <MovieMaker713@gmail.com>
2013-11-23 08:23:15 -0800

    Updating the SQL dump for creating tables.

--- sql/create-db.sql
+++ sql/create-db.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Community v11.28 (64 bit)
-MySQL - 5.5.24-log : Database - gbutiri_fbclone
+MySQL - 5.5.24-log : Database - fbclone
 *********************************************************************
 */
 
@@ -26,7 +26,26 @@ CREATE TABLE `posts` (
   `dtm` datetime DEFAULT NULL,
   `poster` varchar(20) DEFAULT 'admin',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `signup` */
+
+DROP TABLE IF EXISTS `signup`;
+
+CREATE TABLE `signup` (
+  `username` varchar(20) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `fname` varchar(20) DEFAULT NULL,
+  `lname` varchar(20) DEFAULT NULL,
+  `bdate` datetime DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `lastlogin` datetime DEFAULT NULL,
+  `added` datetime DEFAULT NULL,
+  `emailverified` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
