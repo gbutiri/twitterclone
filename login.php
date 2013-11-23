1d1e485b1d0bd54ffdc30491d20b0e18b99198ab
George Butiri <MovieMaker713@gmail.com>
2013-11-23 14:21:54 -0800

    More translation.

--- login.php
+++ login.php
@@ -2,10 +2,13 @@
 include ('config.php');
 include (_DOCROOT.'/includes/header.php');
 ?>
+<div class="main-billboard">
+Conectați-vă cu prieteni Români!
+</div>
 <form method="POST" id="signin-form" class="registration-forms" action="/ajax/registration.php?action=trylogin">
 	<label>Nume de utilizator:</label>
 	<div><input type="text" name="signin-username" id="signin-username" /></div>
-	<label>Parola:</label>
+	<label>Parolă:</label>
 	<div><input type="password" name="signin-password" id="signin-password" /></div>
 	<div><button id="signin-button" />Conectați-vă</button></div>
 </form>
@@ -14,7 +17,7 @@ include (_DOCROOT.'/includes/header.php');
 	<div><input type="text" name="signup-email" id="signup-email" /></div>
 	<label>Nume de utilizator:</label>
 	<div><input type="text" name="signup-username" id="signup-username" /></div>
-	<label>Parola:</label>
+	<label>Parolă:</label>
 	<div><input type="password" name="signup-password" id="signup-password" /></div>
 	<div><button type="submit" id="signup-button" />Înregistrați-vă</button></div>
 </form>
