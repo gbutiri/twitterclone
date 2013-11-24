03002fb0cd37d72af71b555749a80a828340b2f9
George Butiri <MovieMaker713@gmail.com>
2013-11-24 11:01:38 -0800

    fixed file format

--- profile.php
+++ profile.php
@@ -8,7 +8,7 @@ $profile_un = $_GET['un'];
 ?>
 <h1><?php echo $profile_un; ?></h1>
 <ul id="tweets" data-username="<?php echo $profile_un; ?>"></ul>
-<button id="load-more">�ncarca?i mai multe mesaje (<span><?php echo $numMore-10; ?></span>)</button>
+<button id="load-more">Încărcați mai multe mesaje (<span><?php echo $numMore-10; ?></span>)</button>
 <?php
 
 include (_DOCROOT.'/includes/footer.php');
