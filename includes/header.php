c5cb5cd468fc01dbf300ed34e277d4a016dbcefd
George Butiri <MovieMaker713@gmail.com>
2013-11-30 17:55:45 -0800

    adding google analytics

--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,23 @@
 	<script src="/js/main.js"></script>
 </head>
 <body>
+<?php 
+$servers = array('127.0.0.1','4.30.56.71','68.5.250.174');
+//echo($_SERVER['REMOTE_ADDR']);
+if (!in_array($_SERVER['REMOTE_ADDR'],$servers)) {
+?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-2397724-41', 'ceau.ro');
+  ga('send', 'pageview');
+</script>
+<?php
+}
+?>
 <div class="page-wrapper">
 <nav>
 	<a class="logo" href="/">ceau<span>.ro</span></a>
